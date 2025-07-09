0fa4f8b7ba8b452944d08f06c7f5cbec12540359
cairocarrilho <cairojunior@gmail.com>
2025-07-09 05:01:28 -0300

    feat/Criando estrutura do back end

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,17 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
 const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+
+
+
+
+const Port = 3000;
+app.listen(Port, () => {
+    console.log(`Servidor rodando na porta ${Port}`);
+});
